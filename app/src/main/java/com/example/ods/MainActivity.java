4264d843fa0a6c8a14bd8c1fba2e72656f6dd38f
EGMJ <gabrieledson730@gmail.com>
2024-10-23 22:28:04 -0300

    feat: add fragments and bottom navigation

--- app/src/main/java/com/example/ods/MainActivity.java
+++ app/src/main/java/com/example/ods/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.ods;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.ods.fragments.ExerciseFragment;
+import com.example.ods.fragments.HistoryFragment;
+import com.example.ods.fragments.HomeFragment;
+import com.example.ods.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Map<Integer, Fragment> fragmentMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        fragmentMap = new HashMap<>();
+        fragmentMap.put(R.id.nav_home, new HomeFragment());
+        fragmentMap.put(R.id.nav_history, new HistoryFragment());
+        fragmentMap.put(R.id.nav_exercise, new ExerciseFragment());
+        fragmentMap.put(R.id.nav_settings, new SettingsFragment());
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, Objects.requireNonNull(fragmentMap.get(R.id.nav_home))).commit();
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = fragmentMap.get(item.getItemId());
+
+                if (selectedFragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, selectedFragment).commit();
+                }
+                return true;
+            }
+        });
+
     }
+
 }
