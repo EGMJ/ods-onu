ed583fd9d0c188a83de4ec533efc93edd4a9eb71
EGMJ <gabrieledson730@gmail.com>
2024-11-05 04:55:18 -0300

    feat: add tela cadastro, mais func e layout das outras telas

--- app/src/main/java/com/example/ods/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/ods/fragments/SettingsFragment.java
@@ -1,26 +1,41 @@
 package com.example.ods.fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import com.example.ods.R;
-
 
 public class SettingsFragment extends Fragment {
 
+//    private Button btnSair;
+
     public SettingsFragment() {
-        // Required empty public constructor
+        // Construtor público vazio necessário
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        // Infla o layout para este fragmento
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+//        // Referência para o botão "Sair"
+//        btnSair = view.findViewById(R.id.btnSair);
+//
+//        // Configuração do clique do botão "Sair"
+//        btnSair.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Encerra a atividade que contém o fragmento
+//                if (getActivity() != null) {
+//                    getActivity().finish();
+//                }
+//            }
+//        });
+//
+        return view; // Retorna a view inflada
     }
 }
