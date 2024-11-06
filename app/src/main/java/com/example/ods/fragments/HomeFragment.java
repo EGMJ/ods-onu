e931a6182c73105063e4c2507db135b0e74da9a6
EGMJ <gabrieledson730@gmail.com>
2024-11-06 17:13:38 -0300

    feat: finalizando tela home

--- app/src/main/java/com/example/ods/fragments/HomeFragment.java
+++ app/src/main/java/com/example/ods/fragments/HomeFragment.java
@@ -1,16 +1,22 @@
 package com.example.ods.fragments;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ods.HistoricoMed;
+import com.example.ods.LoginActivity;
 import com.example.ods.R;
 
 public class HomeFragment extends Fragment {
+    private CardView cardView;
 
     public HomeFragment() {}
 
@@ -18,6 +24,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cardView = view.findViewById(R.id.cardFormulario);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(getActivity(), HistoricoMed.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
